3a2c68d825d7e936eec538217562aa3365ce2f20
marcin <m.skwarek@stud.elka.pw.edu.pl>
2017-01-03 22:42:06 +0100

    more pythonic way

--- awgn.py
+++ awgn.py
@@ -7,12 +7,6 @@ def read_signal_data(filename):
 def generate_noise(length):
     return numpy.random.normal(0,1,length)
 
-def add_noise_to_signal(signal, noise):
-    signal_with_noise = []
-    for i in range(0, len(signal)):
-        signal_with_noise.append( float(signal[i]) + noise[i]) 
-    return signal_with_noise
-
 if __name__=='__main__':
     signal = read_signal_data('dane')
     noise = generate_noise(len(signal))
