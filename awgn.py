1a9eaa4de0a5ed1b4dcd92931d182c84fcdd76a2
mskwarek <m.skwarek@stud.elka.pw.edu.pl>
2017-01-06 19:05:17 -0500

    added awg noise generation

--- awgn.py
+++ awgn.py
@@ -1,17 +1,46 @@
+import argparse
 import numpy
+
 
 def read_signal_data(filename):
     with open(filename, 'r') as fi:
         return list(fi.read())
 
-def generate_noise(length):
-    return numpy.random.normal(0,1,length)
 
-if __name__=='__main__':
-    signal = read_signal_data('dane')
-    noise = generate_noise(len(signal))
-    signal_with_noise = [float(signal[i])+noise[i] for i in range(0, len(signal))]
-    print "noise:", noise    
-    print "signal:", signal
-    print "signal with noise", signal_with_noise
-    print "len: ", len(signal_with_noise), len(signal), len(noise)
+def write_noised_signal_data(filename, noised):
+    formatted = [str(noised[i]) for i in range(0, len(noised))]
+    with open(filename, 'w+') as fi:
+        fi.write("\n".join(formatted))
+
+
+def measure_power(x):
+    return float(sum([(float(1) / float(len(x))) * float(x[i]) ** 2 for i in range(0, len(x))]))
+
+
+def generate_random_vector(signal):
+    return numpy.random.normal(0, 1, len(signal))
+
+
+def convert_snr_to_lin(snr):
+    return float(10 ** (snr / 20))
+
+
+def noise_signal(orig_signal, snr):
+    variance = float(numpy.sqrt(measure_power(orig_signal) / convert_snr_to_lin(snr)))
+    noise = variance * generate_random_vector(orig_signal)
+    return [float(orig_signal[i]) + noise[i] for i in range(0, len(orig_signal))]
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--src', type=str, required=True, dest='src')
+    parser.add_argument('--dst', type=str, required=True, dest='dst')
+    parser.add_argument('--snr', type=float, required=True, dest='snr')
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    signal = read_signal_data(parse_arguments().src)
+    signal_with_noise = noise_signal(signal, parse_arguments().snr)
+    write_noised_signal_data(parse_arguments().dst, signal_with_noise)
+
--- awgn_unit_test.py
+++ awgn_unit_test.py
@@ -1,10 +1,25 @@
 import unittest
+
 import awgn
+
 
 class IsOddTests(unittest.TestCase):
     
     def testOne(self):
         self.failUnless(len(awgn.read_signal_data('dane')) > 0)
+
+    def testSum(self):
+        self.failUnless(awgn.measure_power([2, 2, 2, 2]) is not 4)
+
+    def testNoise(self):
+        import matplotlib.pyplot as plot
+        import numpy as np
+        x = np.linspace(-2*np.pi, 2*np.pi, 500)
+        y = 10*np.sin(x)
+        z = awgn.noise_signal(y, 30)
+        plot.plot(x, y)
+        plot.plot(x, z)
+        plot.show()
 
 def main():
     unittest.main()
