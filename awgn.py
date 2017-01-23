6e9da16cd292860f69501281c64110061c6c4e37
mskwarek <m.skwarek@stud.elka.pw.edu.pl>
2017-01-23 16:44:37 -0500

    * changed to complex input

--- awgn.py
+++ awgn.py
@@ -2,25 +2,26 @@ import argparse
 import numpy
 
 
-''' module for AWGN channel simulation simulation '''
-
 def read_signal_data(filename):
-    with open(filename, 'r') as fi:
-        return list(fi.read())
+    with open(filename) as f:
+        content = f.readlines()
+    return [numpy.complex(x.strip()) for x in content]
 
 
 def write_noised_signal_data(filename, noised):
-    formatted = [str(noised[i]) for i in range(0, len(noised))]
+    formatted = [str(noised[i]).replace(")", "").replace("(", "") for i in range(0, len(noised))]
     with open(filename, 'w+') as fi:
         fi.write("\n".join(formatted))
 
 
 def measure_power(x):
-    return float(sum([(float(1) / float(len(x))) * float(x[i]) ** 2 for i in range(0, len(x))]))
+    return float(sum([(float(1) / float(len(x))) * numpy.absolute(x[i]) ** 2 for i in range(0, len(x))]))
 
 
 def generate_random_vector(signal):
-    return numpy.random.normal(0, 1, len(signal))
+    re = numpy.random.normal(0, 1, len(signal))
+    im = numpy.random.normal(0, 1, len(signal))
+    return [numpy.complex(re[x], im[x]) for x in range(0, len(re))]
 
 
 def convert_snr_to_lin(snr):
@@ -28,9 +29,11 @@ def convert_snr_to_lin(snr):
 
 
 def noise_signal(orig_signal, snr):
-    variance = float(numpy.sqrt(measure_power(orig_signal) / convert_snr_to_lin(snr)))
-    noise = variance * generate_random_vector(orig_signal)
-    return [float(orig_signal[i]) + noise[i] for i in range(0, len(orig_signal))]
+    variance = numpy.sqrt(measure_power(orig_signal) / (2 * convert_snr_to_lin(snr)))
+    random_data = generate_random_vector(orig_signal)
+    print len(random_data), variance
+    noise = [random_data[i] * variance for i in range(0, len(random_data))]
+    return [(orig_signal[i]) + noise[i] for i in range(0, len(orig_signal))]
 
 
 def parse_arguments():
@@ -43,6 +46,7 @@ def parse_arguments():
 
 if __name__ == '__main__':
     signal = read_signal_data(parse_arguments().src)
+    print signal
     signal_with_noise = noise_signal(signal, parse_arguments().snr)
+    print signal_with_noise
     write_noised_signal_data(parse_arguments().dst, signal_with_noise)
-
